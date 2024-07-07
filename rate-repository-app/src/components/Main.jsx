8689fed407346fa27293b5808a3bb8d5ed8e4932
Juho Kangas <juho.kangas@aalto.fi>
2024-07-07 16:26:46 +0300

    platform specific fonts 10.10

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,14 +1,19 @@
+import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Route, Routes, Navigate } from 'react-router-native'
 import RepositoryList from './RepositoryList'
 import AppBar from './AppBar'
 import SignIn from './SignIn'
+import theme from '../theme'
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
     backgroundColor: '#e1e4e8'
+  },
+  content: {
+    fontFamily: theme.fonts.main
   }
 })
 
@@ -16,11 +21,13 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <Routes>
-        <Route path='/' element={<RepositoryList />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='*' element={<Navigate to='/' replace />} />
-      </Routes>
+      <View style={styles.content}>
+        <Routes>
+          <Route path='/' element={<RepositoryList />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </View>
     </View>
   )
 }
